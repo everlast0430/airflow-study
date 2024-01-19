d86d22993e423798d44ff6a0d77a6852d360d2ac
jinwoo <gescheit00@naver.com>
2024-01-19 17:38:43 +0900

    task_decorator 업로드수정

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 
 with DAG(
     dag_id="dags_python_task_decorator",
-    schedule="0 2 0 0 1",
+    schedule="0 2 * * 1",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
