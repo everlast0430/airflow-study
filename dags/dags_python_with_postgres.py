bad611b5bb2423585a499a8752db8a4e45e6aba7
jinwoo <sabrikong@gmail.com>
2024-08-04 16:23:20 +0900

    docker yaml upload

---
+++ dags/dags_python_with_postgres.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id='dags_python_with_postgres',
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+
+    def insrt_postgres(ip, port, dbname, user, passwd, **kwargs):
+        import psycopg2
+        from contextlib import closing
