de1053d34bf959c87b904859164f93bf17e25cc5
jinwoo <sabrikong@gmail.com>
2024-08-04 18:02:05 +0900

    python_with_postgres.py upload4

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -3,12 +3,11 @@ import pendulum
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    dag_id='dags_python_with_postgres',
-    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
-    schedule=None,
-    catchup=False
+        dag_id='dags_python_with_postgres',
+        start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+        schedule=None,
+        catchup=False
 ) as dag:
-
     def insrt_postgres(ip, port, dbname, user, passwd, **kwargs):
         import psycopg2
         from contextlib import closing
@@ -18,10 +17,11 @@ with DAG(
                 dag_id = kwargs.get('ti').dag_id
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
-                msg = 'insert 수행'
-                sql = 'insert into postgres.public.py_opr_drct_insrt values (%s,%s,%s,%s);'
-                cursor.execute(sql, (dag_id,task_id,run_id,msg))
+                msg = 'insrt 수행'
+                sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
+                cursor.execute(sql, (dag_id, task_id, run_id, msg))
                 conn.commit()
+
 
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
