3c689768bef9872e781d3dbed3aa0645c7e01e62
jinwoo <sabrikong@gmail.com>
2024-08-15 19:56:11 +0900

    bulk load modify

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -5,7 +5,7 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 with DAG(
     dag_id='dags_python_with_postgres_hook_bulk_load',
-    start_date=pendulum.datetime(2023,05,01, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2023,5,1, tz='Asia/Seoul'),
     schedule='0 7 * * *',
     catchup=False
 ) as dag:
