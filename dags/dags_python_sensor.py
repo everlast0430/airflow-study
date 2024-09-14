6bed7828b733136db52f5d374f026d8b84e323dc
sabrikong <sabrikong@gmail.com>
2024-09-14 09:12:45 +0900

    python_sensor.py upload2

--- dags/dags_python_sensor.py
+++ dags/dags_python_sensor.py
@@ -22,4 +22,28 @@ with DAG(
         row_data = contents.get(key_nm).get('row')
         last_dt = row_data[0].get(base_dt_col)
         last_date = last_date.replace('.','_').replace('/','-')
+        try:
+            pendulum.from_format(last_date, 'YYYY-MM-DD')
+        except:
+        from airflow.exceptions import AirflowException
+        AirflowException(f'{base_dt_col} 컬럼은 YYYY.MM.DD 또는 YYYY/MM/DD 형태가 아닙니다.')
+
+    today_ymd = kwargs.get('data_interval_end').in_timezone('Asia/Seoul').strftime('%Y-%m-%d')
+    if last_date >= today_ymd:
+        print(f'생성 확인(배치 날짜: {today_ymd} / API Last 날짜: {last_date})')
+        return True
+    else:
+        print(f'Update 미완료 (배치 날짜: {today_ymd} / API Last 날짜:{last_date})')
+        return False
+
+    sensor_task = PythonSensor(
+        task_id='sensor_task',
+        python_callable=check_api_update,
+        op_kwargs={'http_conn_id': 'openapi.seoul.go.kr',
+                   'endpoint': '{{var.value.apikey_openapi_seoul_go_kr}}/json/TbCorona19CountStatus',
+                   'base_dt_col': 'S_DT'},
+        poke_interval=600,  # 10분
+        mode='reschedule'
+    )
+
 
